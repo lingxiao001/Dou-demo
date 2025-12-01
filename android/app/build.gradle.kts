1d68557002c4efc16a74fcbcd811cb8c5e366e71
lingxiao001 <lingxiao98899@gmail.com>
2025-12-01 15:15:43 +0800

    finish step1:添加依赖 ，新加activity_mian.xml, 配置了全屏的ViewPager2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,3 +42,10 @@ android {
 flutter {
     source = "../.."
 }
+
+dependencies {
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}
+
