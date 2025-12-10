ab307844fa9a07de10151314d2bc29b6d10d7478
lingxiao001 <lingxiao98899@gmail.com>
2025-12-10 13:25:15 +0800

    加速 （预加载）flutter视频内流

--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,45 @@ import 'package:douyin_demo/features/feed/views/feed_screen.dart';
 import 'package:douyin_demo/features/viewer/views/viewer_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:douyin_demo/common/providers/app_providers.dart';
+import 'package:douyin_demo/common/services/video_asset_cache_service.dart';
+import 'package:flutter/foundation.dart';
 
 void main() {
   runApp(ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) => _warmUp());
+  }
+
+  Future<void> _warmUp() async {
+    try {
+      final repo = ref.read(videoRepositoryProvider);
+      final posts = await repo.fetchVideoPosts();
+      if (!kIsWeb) {
+        final svc = VideoAssetCacheService();
+        final toPrefetch = posts.take(5).map((p) => p.videoUrl).toList();
+        await svc.prefetch(toPrefetch);
+      }
+    } catch (_) {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       onGenerateRoute: (settings) {
